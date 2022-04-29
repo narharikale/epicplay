68cc5ab0f64bb544056e9f386f3e9b355598cb1f
narharikale <naraharikale5051@gmail.com>
2022-04-29 13:21:57 +0530

    refactor : unused code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Header} from './frontend/component';
 import { Routers } from './frontend/Router/Router';
-import { Home } from './frontend/screen';
 
 
 function App() {
