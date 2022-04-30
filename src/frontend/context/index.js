26c26e0b050c3a222149365648529648952c6b32
narharikale <naraharikale5051@gmail.com>
2022-04-30 19:26:37 +0530

    refactor

--- src/frontend/context/index.js
+++ src/frontend/context/index.js
@@ -1,4 +1,4 @@
-export { VideoProvider , useVideo } from './videos-context/Videos-Context';
+export { VideoProvider , useVideo } from './videos-context/Videos-Context'
 export { AuthProvider , useAuth }  from './auth-context/Auth-context';
 export { ThemeProvider , useTheme } from './theme-context/Theme-Context';
 export { FilterProvider , useFilter } from './filter-context/Filter-context'
