5a72b7dcb7db34e511b6aae6f74b30cd8e9b9b0f
narharikale <naraharikale5051@gmail.com>
2022-04-30 19:58:44 +0530

    refactor

--- src/frontend/screen/index.js
+++ src/frontend/screen/index.js
@@ -1,5 +1,3 @@
-export { Home } from './Home/Home';
-export { Signin } from './auth/Signin'; 
-export { Signup } from './auth/Signup';
-export { Explore } from './Explore/Explore';
-export { NotFound } from './NotFound/NotFound'
+export { Home } from "./Home/Home";
+export { Signin } from "./auth/Signin"
+export { Signup } from "./auth/Signup"
