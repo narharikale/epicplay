f85a7c7179c50890743639480d38dbbf787cfeb2
narharikale <naraharikale5051@gmail.com>
2022-04-30 20:21:14 +0530

    reafactor

--- src/frontend/screen/index.js
+++ src/frontend/screen/index.js
@@ -4,7 +4,7 @@
 
 
 export { Home } from "./Home/Home";
-export { Signin }  from "./auth/Signin"
-export { Signup } from "./auth/Signup"
-export { Explore } from "./Explore/Explore"
-export { NotFound } from "./NotFound/NotFound"
+export { Signin } from "./auth/Signin";
+export { Signup } from "./auth/Signup";
+export { Explore } from "./Explore/Explore";
+export { NotFound } from "./NotFound/NotFound";
