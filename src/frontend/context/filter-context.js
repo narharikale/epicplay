66213ac5fee833dc3bd6edb42e1347bf331dca08
narharikale <naraharikale5051@gmail.com>
2022-05-10 23:34:33 +0530

    refactor

--- src/frontend/context/filter-context.js
+++ src/frontend/context/filter-context.js
@@ -1,15 +1,17 @@
-import { createContext, useContext } from "react"
-import { useReducer } from "react"
-import { filterReducer } from "../Reducers/filterReducer"
+import { createContext, useContext , useReducer } from "react"
+import { filterReducer } from '../Reducers/filterReducer'
 
 const FilterContext = createContext(null);
 
 function FilterProvider( { children }){
 
+        
     const [filterState , filterDispatch] = useReducer( filterReducer , { category:'All' })
+    
 
     return (
         <FilterContext.Provider value ={{ filterDispatch , filterState }}>
+            
             { children }
         </FilterContext.Provider>
     )
