dc682a13959ff7c32730ab47d3ef23b652d93271
narharikale <naraharikale5051@gmail.com>
2022-05-11 18:38:20 +0530

    refactor

--- src/frontend/Router/Router.js
+++ src/frontend/Router/Router.js
@@ -1,13 +1,10 @@
 import { Navigate, Route, Routes , } from "react-router-dom";
-import Mockman from 'mockman-js';
 import { Home , Signin , Signup , Explore, NotFound, SingleVideo, WatchLater, LikedVideos, History, Playlist } from "../screen";
-import { useAuth } from '../context'
 import { RequireAuth } from "../component";
 
 
 
 function Routers(){
-    const { isAuth } = useAuth(); 
     return(
         <Routes>
             <Route path="/" element={<Home/>}/>
@@ -42,7 +39,7 @@ function Routers(){
 
             <Route path="/notfound" element={<NotFound/>}/>
             <Route path="*" element={<Navigate to='/notfound' replace/> } />
-            <Route path="/mock" element={<Mockman/>} />
+            
         </Routes>
     )
 }
